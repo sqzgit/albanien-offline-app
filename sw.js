e3c99bac337874a653eeff502372d89112c9b8c1
jediminded <kevin.hansmann@outlook.de>
2026-06-17 21:14:36 +0200

    Upload basic framework

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'albanien-guide-v1';
+// Hier alle Dateien eintragen, die offline verfügbar sein müssen
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon.png',
+  'icon-512.png'
+];
+
+// Beim Installieren der App werden die Dateien in den Cache geladen
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Dateien werden gecacht');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivierung und Löschen alter Caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Netzwerk-Anfragen abfangen: Erst im Cache suchen, sonst Netzwerk
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
