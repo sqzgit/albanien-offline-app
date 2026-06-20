0dbb996ee1b7ff7ef381e871cfa4dd9e554b4abd
sqzgit <kevin.hansmann@outlook.de>
2026-06-20 22:59:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'albanien-guide-v10';
+const CACHE_NAME = 'albanien-guide-v11';
 
 // Hier alle Dateien eintragen, die offline verfügbar sein müssen (mit ./ für stabileres Routing)
 const ASSETS = [
